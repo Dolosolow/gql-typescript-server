9d37f6e9c5d310bc405d0fec48d62f3af3debc70
Dolosolow <Jose.Munoz07c@gmail.com>
2021-05-12 11:58:22 -0400

    Setup jest for testing. Implemented a "Register user" validating everything working good.

---
+++ src/__tests__/register.test.ts
@@ -0,0 +1,36 @@
+import { request } from "graphql-request";
+import { Connection, createConnection } from "typeorm";
+
+import { User } from "../entity/User";
+
+const host = "http://localhost:4000";
+const email = "jochy07c@gmail.com";
+const password = "test123.";
+
+const mutation = `
+  mutation {
+    register(email: "${email}", password: "${password}")
+  }
+`;
+
+let connection: Connection;
+
+beforeAll(async () => {
+  connection = await createConnection();
+});
+
+afterAll(async () => {
+  await connection.close();
+});
+
+test("Register user", async () => {
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
