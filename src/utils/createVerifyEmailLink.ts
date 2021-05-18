a8dbe31fe6ff0ca10e83cb57dbfdfadae11702e5
Dolosolow <Jose.Munoz07c@gmail.com>
2021-05-18 15:27:30 -0400

    Removed white space.

--- src/utils/createVerifyEmailLink.ts
+++ src/utils/createVerifyEmailLink.ts
@@ -3,6 +3,6 @@ import { v4 as uuidv4 } from "uuid";
 
 export const createVerifyEmailLink = async (url: string, userId: string, redis: Redis) => {
   const id = uuidv4();
-  await redis.set(id, userId, "ex", 60 * 60 * 23);
+  await redis.set(id, userId, "ex", 60 * 60 * 24);
   return `${url}/confirm/${id}`;
 };
