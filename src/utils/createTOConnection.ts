e722fd59d4bb4935e48828ae33956907843119f1
Dolosolow <Jose.Munoz07c@gmail.com>
2021-05-25 09:39:35 -0400

    Fixed heroku configuration, setup pg + redis. squash commits.

--- src/utils/createTOConnection.ts
+++ src/utils/createTOConnection.ts
@@ -1,6 +1,11 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
 export const createTOConnection = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  let connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+
+  if (process.env.NODE_ENV === "production") {
+    connectionOptions = Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  }
+
   return createConnection({ ...connectionOptions, name: "default" });
 };
